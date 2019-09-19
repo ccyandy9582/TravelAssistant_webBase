5f8124b64459b2e8e1bbab43e4a81c17454eab41
nextson123456 <180086825@stu.vtc.edu.hk>
2019-09-19 16:14:11 +0800

    19092019_V1

--- index.php
+++ index.php
@@ -11,6 +11,19 @@
             <a class="menu_item">Login</a>
         </div>
         <div id="main">
+            <div id="desc" >
+                <div>Learn Git and GitHub without any code!<br>
+                Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.</div>
+                <a>Plan your trip</a> <a>explore others</a>
+            </div>
+            <div><h2>Recommand </h2></div>
+            <div><h2>Recommand </h2></div>
+            <div><h2>Recommand </h2></div>
+            <div><h2>Recommand </h2></div>
+            <div><h2>Recommand </h2></div>
         </div>
+        <noscript style="z-index:21;top:0;position:fixed;background-color:cyan; height:100%;width:100%">
+            <center style="position:relative;top:50%">ERROR: Please revive your javascript</center>
+        </noscript>
     </body>
 </html>
