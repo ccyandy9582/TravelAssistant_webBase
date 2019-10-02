b5b12dfa1abaffafb1e1a2cfd3424f508a331ada
nextson123456 <180086825@stu.vtc.edu.hk>
2019-10-02 17:25:06 +0800

    banner

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php require("html_start.php");?>
 <div id="banner_container" >
-    <div id="banner"></div>
-    <div id="banner_content">Learn Git and GitHub without any code!<br>
-    Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.<br><br><br>
-    <a href="create.php">Plan your trip</a> <a>explore others</a></div>
+    <div id="banner_content">
+        PLAN YOUR TRIP
+        <div>
+            test
+        </div>
+    </div>
 </div>
 <div><h2>Recommand </h2></div>
 <div><h2>Recommand </h2></div>
--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <?php require("html_start.php")?>
     <div id="login_background" src="imgs/login.jpg">
     <form id="login">
-        email: <input><br>
-        password: <input type="password"><br>
+        <table border="0">
+            <tr><td>email:</td><td><input></td></tr>
+            <tr><td>password:</td><td><input type="password"></td></tr>
+        </table>
         <a style="color:blue">forgot password?</a><br>
         <button>login</button>
     </form>
