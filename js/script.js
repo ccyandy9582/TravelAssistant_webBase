285ae742a19fab7ef670878a5fe208727af05117
nextson123456 <180086825@stu.vtc.edu.hk>
2019-10-03 10:46:46 +0800

    login

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 $("document").ready(function() {
+    $("#login_switch").click(function() {
+        $(this).toggleClass("login_left").css("animation-duration","2s");
+    })
 })
--- login.php
+++ login.php
@@ -1,11 +1,15 @@
 <?php require("html_start.php")?>
     <div id="login_background" src="imgs/login.jpg">
-    <form id="login">
-        <table border="0">
-            <tr><td>email:</td><td><input></td></tr>
-            <tr><td>password:</td><td><input type="password"></td></tr>
-        </table>
-        <a style="color:blue">forgot password?</a><br>
-        <button>login</button>
-    </form>
+    <div id="login_container">
+        <div id="login">
+            <form id="login_form">
+                <input type="text" placeholder="email"><br><br>
+                <input type="password" placeholder="password"><br>
+                <a style="color:blue">forgot password?</a><br>
+                <button>login</button>
+            </form>
+        </div
+        ><div id="login_switch">test
+        </div>
+    </div>
 <?php require("html_end.php")?>
