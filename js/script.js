12a9b869926659f440b68c2c816f0237d7bf8a63
nextson123456 <180086825@stu.vtc.edu.hk>
2019-10-03 15:13:28 +0800

    login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $("document").ready(function() {
-    $("#login_switch").click(function() {
-        $(this).toggleClass("login_left").css("animation-duration","2s");
+    $(".login_switch_btn").click(function() {
+        if(!$("#login_switch").hasClass("cd")) {
+            $("#login_switch").toggleClass("on").addClass("cd").css("animation-duration","0.75s");
+            $("#login").toggleClass("on");
+            setTimeout(function() {
+                $("#login_switch").removeClass("cd")
+            },750);
+            $(".login_switch_btn span").each(function() {
+                $(this).toggleClass("hidden");
+            })
+            setTimeout(function() {
+                $("#login form").each(function() {
+                    $(this).toggleClass("hidden");
+                })
+            },375);
+        }
     })
 })
--- login.php
+++ login.php
@@ -1,15 +1,26 @@
 <?php require("html_start.php")?>
     <div id="login_background" src="imgs/login.jpg">
     <div id="login_container">
-        <div id="login">
+        <table id="login">
+            <tr><td>
+            <a style="color:blue; position: absolute; bottom:0; left:0">forgot password?</a>
             <form id="login_form">
                 <input type="text" placeholder="email"><br><br>
-                <input type="password" placeholder="password"><br>
-                <a style="color:blue">forgot password?</a><br>
+                <input type="password" placeholder="password"><br><br>
                 <button>login</button>
             </form>
-        </div
-        ><div id="login_switch">test
-        </div>
+            <form id="reg_form" class=hidden>
+                <input type="text" placeholder="email"><br><br>
+                <input type="password" placeholder="password"><br><br>
+                <input type="password" placeholder="confirm password"><br><br>
+                <button>register</button>
+            </form>
+        </td></tr>
+        </table
+        ><table id="login_switch">
+            <tr><td>
+            <button class="login_switch_btn"><span>register</span><span class="hidden">login</span></button>
+            </td></tr>
+        </table>
     </div>
 <?php require("html_end.php")?>
