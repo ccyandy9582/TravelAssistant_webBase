3caf7434f0cf3a485efb3db794a135d551e66cd4
nixonwwl <nixon123465@gmail.com>
2019-10-08 23:51:26 +0800

    meta

--- html_start.php
+++ html_start.php
@@ -3,6 +3,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="/fyp/js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="/fyp/css/style.css">
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
     </head>
     <body>
         <div id="menu">
