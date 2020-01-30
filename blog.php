0d765764c0cbe7c031612c647ccacc36fbf26dec
nixonwwl <nixon123465@gmail.com>
2020-01-30 17:46:57 +0800

    more UI

--- blog.php
+++ blog.php
@@ -9,17 +9,17 @@
         <option>most popular</option>
     <select><br><br><br>
     <div class="blog_container">
-        <h4>大阪市區景點巡禮</h4>
+        <h4>大阪市區景點巡禮 <span>By user1</span></h4>
         如果你的關西自由行有打算停留滿滿的一天在大阪，推薦可以先從大阪市區幾個比較經典的景點開始玩，由於大阪的交通超方便！所有景點都可以搭乘「大阪地下鐵」到達，如果使用「大阪周遊卡」，這些景點都是免費不用再買門票入場，一整個超划算。
         <br><div class="blog_more"><a href="/fyp/blog?bid=1">Read More&#187;</a></div>
     </div>
     <div class="blog_container">
-        <h4>環球影城一日遊</h4>
+        <h4>環球影城一日遊 <span>By user2</span></h4>
         大阪最值得去的景點，飄兒首推大阪環球影城，縱使一整天都耗在園區裡也玩不膩，裡面設施、美景很多，無論大人小孩都可以玩得很盡興，如果晚上還有體力，就去道頓崛或梅田逛逛、吃美食都來得及，由於大阪環球影城要注意的事情太多，大家可以直接點下面文章參考：
         <br><div class="blog_more"><a>Read More&#187;</a></div>
     </div>
     <div class="blog_container">
-        <h4>大阪購物逛街景點</h4>
+        <h4>大阪購物逛街景點 <span>By user3</span></h4>
         購物行程沒什麼好安排的，飄兒直接提供大阪最好買的三個區域給大家，每個商業區可以逛的重點都不一樣，大家直接找自己想逛的去，如果一天想去三個也不是問題，交通方式都是靠大阪地下鐵或是JR就可以到了。
         <br><div class="blog_more"><a>Read More&#187;</a></div>
     </div>
---
+++ resultPage.php
@@ -0,0 +1,94 @@
+<?php require("html_start.php")?>
+    <br>
+    <div class="day_container">
+        <h4>Day 1 <span>starting time: 13:30</span></h4>
+        <table>
+            <tr>
+                <td><img src="/fyp/imgs/route/OsakaAirport.jpg"></td>
+                <td><b>Osaka Airport</b><br>spend time: 10 min<br>Type: starting point</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 46 min<br>View route</td>
+            </tr>
+            <tr>
+                <td><img src="/fyp/imgs/route/Shinsaibashi.JPG"></td>
+                <td><b>Shinsaibashi and Dotonbori</b><br>spend time: 4 hour<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 15 min<br>View route</td>
+            </tr>
+            <tr>
+                <td><img src="/fyp/imgs/route/HARUKAS300.jpg"></td>
+                <td><b>HARUKAS 300</b><br>spend time: 1 hour<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 15 min<br>View route</td>
+            </tr>   
+        </table>
+    </div>
+    <div  class="hotel_container">
+        <table>
+            <tr>
+                <td><img src="/fyp/imgs/route/hotel.jpg"></td>
+                <td><b>Hotel Granvia Osaka</b></td>
+            </tr>
+        </table>
+    </div>
+    <div class="day_container">
+        <h4>Day 2 <span>starting time: 09:00</span></h4>
+        <table>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 15 min<br>View route</td>
+            </tr>   
+            <tr>
+                <td><img src="/fyp/imgs/route/OsakaCastle.jpg"></td>
+                <td><b>Osaka Castle</b><br>spend time: 2 hour<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 29 min<br>View route</td>
+            </tr>   
+            <tr>
+                <td><img src="/fyp/imgs/route/Shinsaibashi.JPG"></td>
+                <td><b>Shinsaibashi and Dotonbori</b><br>spend time: 4 hour 30 min<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 4 min<br>View route</td>
+            </tr>   
+            <tr>
+                <td><img src="/fyp/imgs/route/TempozanKaiyukan.JPG"></td>
+                <td><b>Tempozan Kaiyukan</b><br>spend time: 2 hour 30 min<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 37 min<br>View route</td>
+            </tr>   
+            <tr>
+                <td><img src="/fyp/imgs/route/Umeda.jpg"></td>
+                <td><b>Umeda Sky Building</b><br>spend time: 2 hour<br>Type: Attraction</td>
+            </tr>
+        </table>
+    </div>
+    <div class="day_container">
+        <h4>Day 3 <span>starting time: 10:00</span></h4>
+        <table>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 15 min<br>View route</td>
+            </tr>   
+            <tr>
+                <td><img src="/fyp/imgs/route/Universal.jpg"></td>
+                <td><b>Universal Studios Japan</b><br>spend time: 10 hour<br>Type: Attraction</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><b>Travel</b><br>spend time: 37 min<br>View route</td>
+            </tr>   
+        </table>
+    </div>
+<?php require("html_end.php")?>
